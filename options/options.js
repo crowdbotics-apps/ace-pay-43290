c755db92b752eb37986ddfef770dd4cad0ba0c0c
Crowdbotics <team@crowdbotics.com>
2023-09-08 21:33:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ace-pay-43290",
+  url: "https://ace-pay-43290.botics.co",
+  api: "https://ace-pay-43290.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
